166eca4085550588bf2d868dadb3d73216ad2c43
wykopowiedz <wykopowiedz@wp.pl>
2020-08-22 21:10:04 +0200

    add status

--- resources/js/game.js
+++ resources/js/game.js
@@ -29,6 +29,12 @@ const enemies = {
 
 const enemiesArr = [enemies.POLKA, enemies.HONKHONK, enemies.CHAD]
 
+const status = {
+    READY:    { type: 'READY', msg: 'Ready?'},
+    FIGHT:    { type: 'FIGHT', msg: 'Fight!'},
+    LOST:     { type: 'LOST', msg: 'You lose.'}
+}
+
 
 // var ------------------------------------------------------------
 var currentEnemy
@@ -44,14 +50,17 @@ const gun_div = document.getElementById("gun");
 
 paper_div.addEventListener('click', function() {
     setCurrentFren(frens.PAPER)
+    setStatus(status.FIGHT)
 })
 
 hammer_div.addEventListener('click', function() {
     setCurrentFren(frens.HAMMER)
+    setStatus(status.FIGHT)
 })
 
 gun_div.addEventListener('click', function() {
     setCurrentFren(frens.GUN)
+    setStatus(status.FIGHT)
 })
 
 function hoverElem(elem) {
@@ -60,7 +69,7 @@ function hoverElem(elem) {
 
 
 // backend --------------------------------------------------------
-function game() {
+async function game() {
     while(!isLost) {
         await battle()
     }
@@ -69,26 +78,41 @@ function game() {
 async function battle() {
     var sleepMs = 5000
 
+    setStatus(status.READY)
+
     setCurrentEnemy(randomEnemy())
     console.log("currentEnemy: ", currentEnemy)
 
     console.log("You got " + sleepMs + " millis to choose a fren.")
 
     await sleep(sleepMs)
+
+    console.log("currentFren: ", currentFren)
     fight()
 }
 
 const fight = () => {
-    console.log("currentFren: ", currentFren)
-
-    const isVictory = checkIsVictory(currentFren, currentEnemy)
-    console.log("isVictory: ", isVictory)
-    this.isLost = !isVictory
-    this.winScore += isVictory
+    if (isFrenWin(currentFren, currentEnemy)) {
+        won()
+    } else {
+        lost()
+    }
 }
 
-function checkIsVictory(fren, enemy) {
-    return fren.power + enemy.power == 0;
+function won() {
+    console.log("Victory!")
+    this.winScore ++
+    document.getElementById('user-score').innerHTML = winScore
+}
+
+function lost() {
+    console.log("Lost.")
+    setStatus(status.LOST)
+    this.isLost = true
+}
+
+function isFrenWin(fren, enemy) {
+    return fren != null && fren.power + enemy.power == 0;
 }
 
 function randomEnemy() {
@@ -105,6 +129,10 @@ function setCurrentFren(fren)  {
 function setCurrentEnemy(enemy)  {
     hoverElem(enemy)
     this.currentEnemy = enemy
+}
+
+function setStatus(status) {
+    document.getElementById('status').innerHTML = status.msg
 }
 
 function sleep(ms) {
